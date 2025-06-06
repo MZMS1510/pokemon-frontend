822777fc2e2e9a1b9b446e753e10cc88c3dba1a3
Marcos <msmarcondes10@gmail.com>
2025-06-06 08:19:36 -0300

    feat: create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
 export default function Header() {
   return (
     <nav className="flex justify-between items-center p-5">
-      <a className="hover:underline" href="#">
+      <a className="hover:underline" href="/home">
         Home
       </a>
-      <a className="hover:underline" href="#">
+      <a className="hover:underline" href="/pokedex">
         Pokédex
       </a>
-      <a className="hover:underline" href="#">
+      <a className="hover:underline" href="/trainers">
         Trainers
       </a>
-      <a className="hover:underline" href="#">
+      <a className="hover:underline" href="/teams">
         Teams
       </a>
-      <a className="hover:underline" href="#">
+      <a className="hover:underline" href="/items">
         Items
+      </a>
+      <a className="hover:underline" href="/items">
+        Contact
       </a>
     </nav>
   );
