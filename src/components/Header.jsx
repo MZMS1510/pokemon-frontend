495be867c22355be739ea815dfb2276d2535681a
Marcos <msmarcondes10@gmail.com>
2025-06-06 13:05:32 -0300

    feat: improves header responsivity

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,27 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav className="flex justify-between items-center p-5">
-      <Link className="hover:underline" to="/home">
-        Home
-      </Link>
-      <Link className="hover:underline" to="/pokedex">
-        Pokédex
-      </Link>
-      <Link className="hover:underline" to="/trainers">
-        Trainers
-      </Link>
-      <Link className="hover:underline" to="/teams">
-        Teams
-      </Link>
-      <Link className="hover:underline" to="/items">
-        Items
-      </Link>
-      <Link className="hover:underline" to="/items">
-        Contact
-      </Link>
-    </nav>
+    <header className="flex justify-center w-full">
+      <nav className="flex justify-between items-center p-5 max-w-4xl w-full">
+        <Link className="hover:underline" to="/home">
+          Home
+        </Link>
+        <Link className="hover:underline" to="/pokedex">
+          Pokédex
+        </Link>
+        <Link className="hover:underline" to="/trainers">
+          Trainers
+        </Link>
+        <Link className="hover:underline" to="/teams">
+          Teams
+        </Link>
+        <Link className="hover:underline" to="/items">
+          Items
+        </Link>
+        <Link className="hover:underline" to="/items">
+          Contact
+        </Link>
+      </nav>
+    </header>
   );
 }
