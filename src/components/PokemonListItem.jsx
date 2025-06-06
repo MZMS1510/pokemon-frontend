9c79fb50697494e7e262c069851cb16987bd8110
Marcos <msmarcondes10@gmail.com>
2025-06-06 10:35:08 -0300

    feat: update PokemonListItem component styles

--- src/components/PokemonListItem.jsx
+++ src/components/PokemonListItem.jsx
@@ -2,9 +2,19 @@ export default function PokemonListItem({ pokemon = {} }) {
   const { name, id } = pokemon;
 
   return (
-    <li className="flex flex-col w-full justify-center p-4 rounded-lg bg-cyan-600 hover:bg-cyan-700 hover:cursor-pointer transition duration-300">
-      <p>#{id}</p>
-      <p>{name}</p>
+    <li className="flex flex-col mb-4 w-full justify-center p-4 rounded-lg bg-cyan-600 hover:bg-cyan-700 hover:cursor-pointer transition duration-300">
+      <div className="text-2xl flex justify-start gap-5">
+        <p className="opacity-80">#{id}</p>
+        <p>{name}</p>
+      </div>
+      <div className="flex justify-center mt-4 gap-5 text-xl">
+        <p className="border-2 border-white w-1/3 text-center rounded-4xl p-2">
+          Tipo 1
+        </p>
+        <p className="border-2 border-white w-1/3 text-center rounded-4xl p-2">
+          Tipo 2
+        </p>
+      </div>
     </li>
   );
 }
